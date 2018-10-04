d0a02e0ebb039c8e843c19096eeea9afaba17db7
guilherme <guilhermetyamamoto@gmail.com>
2018-10-04 02:29:17 -0300

    fix: formatted each component in the rules from the project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import {Route} from 'react-router-dom'
-import Searchbar from './components/Searchbar'
-import './App.css'
-import BookList from './components/BookList'
+import React from "react";
+import * as BooksAPI from "./BooksAPI";
+import { Route } from "react-router-dom";
+import Searchbar from "./components/Searchbar";
+import "./App.css";
+import BookList from "./components/BookList";
 class BooksApp extends React.Component {
   state = {
-   
-    books :[]
-    
-  }
-  
-  RerenderShelfs = ()=>{
-    BooksAPI.getAll()
-    .then((result)=>{
-      this.setState(()=>({
-        books:result
-      }))
-    })
-  }
-  componentDidMount(){
-  
+    books: []
+  };
+
+  RerenderShelfs = () => {
+    BooksAPI.getAll().then(result => {
+      this.setState(() => ({
+        books: result
+      }));
+    });
+  };
+  componentDidMount() {
     this.RerenderShelfs();
   }
- 
-  
 
   render() {
-    
     return (
       <div className="app">
-      <Route exact path='/'  render={()=>(
-       <BookList books={this.state.books} RerenderShelfs={this.RerenderShelfs}/>
-      )}/>
-      <Route path='/search' render={({history})=>(
-       <Searchbar  onRedirectToMain={()=>{this.RerenderShelfs();history.push('/')}} />
-      )} />
-   
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <BookList
+              books={this.state.books}
+              RerenderShelfs={this.RerenderShelfs}
+            />
+          )}
+        />{" "}
+        <Route
+          path="/search"
+          render={({ history }) => (
+            <Searchbar
+              onRedirectToMain={() => {
+                this.RerenderShelfs();
+                history.push("/");
+              }}
+            />
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
