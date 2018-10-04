0093b6dd2e1a084f95a846368af7feaf03d59fc8
guilherme <guilhermetyamamoto@gmail.com>
2018-10-04 01:56:35 -0300

    feat: select component to change the shelf from a given book

---
+++ src/components/SelectBook.js
@@ -0,0 +1,40 @@
+import React,{Component} from 'react';
+
+
+class SelectBook extends Component{
+
+
+   
+    
+   
+    render(){
+       
+        const {shelf,updateBook,RerenderShelfs} = this.props;
+   
+        return(
+            <select value={shelf}  onChange = {
+                (e)=>{
+               updateBook(e.target.value).then(
+                   data=>{
+
+                    console.log(data)
+                    if(typeof RerenderShelfs ==='function') {
+                        console.log('ok') 
+                        RerenderShelfs()
+                    }
+                   }
+
+               )
+             
+            }}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        )
+    }
+}
+
+export default SelectBook;
